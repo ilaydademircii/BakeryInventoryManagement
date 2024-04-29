fb912fae053103c22e8d16445821e619b6fea74a
zidily <zilaydademirci@gmail.com>
2024-04-29 16:14:39 +0300

    genel arayüz ve ödeme arayüzü eklendi

--- PastaneOtomasyonu/src/controllers/MainFrameController.java
+++ PastaneOtomasyonu/src/controllers/MainFrameController.java
@@ -1,5 +1,63 @@
 package controllers;
 
-public class MainFrameController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import controllers.customers.AddCustomerController;
+import controllers.customers.DeleteCustomerController;
+import controllers.customers.ListingCustomersController;
+import controllers.payments.PaymentController;
+import controllers.payments.ReceivingPaymentController;
+import controllers.payments.RemainingPaymentsController;
+import view.MainFrame;
+
+public class MainFrameController {
+	public static void main(String[] args) {
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+
+		mainFrame.addCustomerMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddCustomerController controller=new AddCustomerController(mainFrame);
+				controller.execute();
+			}
+		});
+
+		mainFrame.deleteCustomerMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DeleteCustomerController controller=new DeleteCustomerController(mainFrame);
+				controller.execute();
+			}
+		});
+		
+		
+		mainFrame.listCustomersMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListingCustomersController controller=new ListingCustomersController(mainFrame);
+				controller.execute();
+			}
+		});
+		
+		mainFrame.paymentMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PaymentController controller=new PaymentController(mainFrame);
+				controller.execute();
+			}
+		});
+		
+		mainFrame.receivingPaymentsMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ReceivingPaymentController controller=new ReceivingPaymentController(mainFrame);
+				controller.execute();
+			}
+		});
+
+		mainFrame.remainingPaymentsMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RemainingPaymentsController controller=new RemainingPaymentsController(mainFrame);
+				controller.execute();
+			}
+		});
+		
+	}
 }
--- PastaneOtomasyonu/src/view/customers/AddCustomerFrame.java
+++ PastaneOtomasyonu/src/view/customers/AddCustomerFrame.java
@@ -1,15 +1,109 @@
 package view.customers;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddCustomerFrame extends JInternalFrame {
-
-//	private static final long serialVersionUID = 1L;
-
+	private JTextField cafeName;
+	public JTextField taxNumber;
+	public JTextField phoneNumber;
+	public JTextField taxAdministration;
+	public JTextArea explanation;
+	public JFormattedTextField dateArea;
+	public JButton addingButton;
+	public JTextArea address;
 
 	public AddCustomerFrame() {
-		setBounds(100, 100, 450, 300);
+		setClosable(true);
+		setMaximizable(true);
+		setIconifiable(true);
+		setBounds(100, 100, 714, 665);
+		getContentPane().setLayout(null);
+
+		cafeName = new JTextField();
+		cafeName.setColumns(10);
+		cafeName.setBounds(207, 29, 240, 30);
+		getContentPane().add(cafeName);
+
+		JLabel label = new JLabel("Kafe Adı");
+		label.setHorizontalAlignment(SwingConstants.LEFT);
+		label.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		label.setBounds(53, 29, 90, 30);
+		getContentPane().add(label);
+
+		taxNumber = new JTextField();
+		taxNumber.setColumns(10);
+		taxNumber.setBounds(207, 79, 240, 30);
+		getContentPane().add(taxNumber);
+
+		JLabel lblNewLabel_1 = new JLabel("Vergi No");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(53, 79, 90, 30);
+		getContentPane().add(lblNewLabel_1);
+
+		JLabel lblNewLabel_1_2 = new JLabel("Vergi Dairesi");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_2.setBounds(53, 130, 112, 30);
+		getContentPane().add(lblNewLabel_1_2);
+
+		JLabel lblTelefonNo = new JLabel("Telefon No");
+		lblTelefonNo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblTelefonNo.setBounds(53, 180, 112, 30);
+		getContentPane().add(lblTelefonNo);
+
+		JLabel lblNewLabel_1_2_3 = new JLabel("Açıklama");
+		lblNewLabel_1_2_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_2_3.setBounds(53, 380, 90, 30);
+		getContentPane().add(lblNewLabel_1_2_3);
+
+		phoneNumber = new JTextField();
+		phoneNumber.setColumns(10);
+		phoneNumber.setBounds(207, 180, 240, 30);
+		getContentPane().add(phoneNumber);
+
+		explanation = new JTextArea();
+		explanation.setBounds(207, 380, 240, 120);
+		getContentPane().add(explanation);
+
+		dateArea = new JFormattedTextField();
+		dateArea.setEnabled(false);
+		dateArea.setBounds(207, 522, 240, 30);
+		getContentPane().add(dateArea);
+
+		JLabel lblNewLabel_1_2_3_1 = new JLabel("Eklenme Zamanı");
+		lblNewLabel_1_2_3_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1_2_3_1.setBounds(53, 521, 144, 30);
+		getContentPane().add(lblNewLabel_1_2_3_1);
+
+		taxAdministration = new JTextField();
+		taxAdministration.setColumns(10);
+		taxAdministration.setBounds(207, 130, 240, 30);
+		getContentPane().add(taxAdministration);
+
+		JLabel lblAddress = new JLabel("Adres");
+		lblAddress.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAddress.setBounds(53, 230, 112, 30);
+		getContentPane().add(lblAddress);
+
+		addingButton = new JButton("Ekle");
+
+		
+		addingButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		addingButton.setBounds(347, 571, 100, 30);
+		getContentPane().add(addingButton);
+
+		address = new JTextArea();
+		address.setBounds(207, 230, 240, 120);
+		getContentPane().add(address);
 
 	}
-
 }
